f2a9bf41af28d1f3ab59dad252f1b3c28ade16cb
majinju <jinjuma@yeah.net>
2019-04-20 13:32:02 +0800

    添加机场员工管理相关功能，雏形基本完成。

--- src/main/resources/doc/db/oracle/myNote.sql
+++ src/main/resources/doc/db/oracle/myNote.sql
@@ -54,11 +54,35 @@ insert into sys_sjgl_tyzd
   (id, cjsj, gxsj, yxx, px, cjrxm, cjrdm, cjrdwmc, cjrdwdm, dm, mc, ms, jp, qp, zdlb, bzzdlb, lbsql, sjly, upnode, cache)
 select id, createdate, etldate, (case when isdel='0' then '1' else '0' end) yxx, px, 
 '管理员','admin','临时机构','99000001',
-dm, mc, ms, jp, qp, zdlb, bzzdlb, lbsql, sjly, upnode, cache from sys_zd_tyzd
+dm, mc, ms, jp, qp, zdlb, bzzdlb, lbsql, sjly, upnode, cache from sys_zd_tyzd;
 
 --文件迁移
 insert into sys_sjgl_file
   (id, cjsj, gxsj, yxx, cjrxm, cjrdm, cjrdwmc, cjrdwdm, ywdm, glid, wjm, qcm, sclb, sclj, wjlx, wjlb)
 select id, createdate, etldate, (case when isdel='0' then '1' else '0' end) yxx,
 '管理员','admin','临时机构','99000001',
- ywdm, glid, wjm, qcm, sclb, sclj, wjlx, wjlb from sys_file_record
+ ywdm, glid, wjm, qcm, sclb, sclj, wjlx, wjlb from sys_file_record;
+
+--修正创建人信息
+update jcga_jcyg_jcxx   t set t.cjrdm='BC5D77315CA84C6C807988E3CD17E70D',t.cjrxm='系统管理员',t.cjrdwdm='141B1AFC7E634176BDA7DB7F491A9004',t.cjrdwmc='临时机构';
+update jcga_jcyg_shgx   t set t.cjrdm='BC5D77315CA84C6C807988E3CD17E70D',t.cjrxm='系统管理员',t.cjrdwdm='141B1AFC7E634176BDA7DB7F491A9004',t.cjrdwmc='临时机构';
+update sys_log_fwzr     t set t.cjrdm='BC5D77315CA84C6C807988E3CD17E70D',t.cjrxm='系统管理员',t.cjrdwdm='141B1AFC7E634176BDA7DB7F491A9004',t.cjrdwmc='临时机构';
+update sys_log_htrz     t set t.cjrdm='BC5D77315CA84C6C807988E3CD17E70D',t.cjrxm='系统管理员',t.cjrdwdm='141B1AFC7E634176BDA7DB7F491A9004',t.cjrdwmc='临时机构';
+update sys_qx_app       t set t.cjrdm='BC5D77315CA84C6C807988E3CD17E70D',t.cjrxm='系统管理员',t.cjrdwdm='141B1AFC7E634176BDA7DB7F491A9004',t.cjrdwmc='临时机构';
+update sys_qx_fwq       t set t.cjrdm='BC5D77315CA84C6C807988E3CD17E70D',t.cjrxm='系统管理员',t.cjrdwdm='141B1AFC7E634176BDA7DB7F491A9004',t.cjrdwmc='临时机构';
+update sys_qx_jgxx      t set t.cjrdm='BC5D77315CA84C6C807988E3CD17E70D',t.cjrxm='系统管理员',t.cjrdwdm='141B1AFC7E634176BDA7DB7F491A9004',t.cjrdwmc='临时机构';
+update sys_qx_jsqxgl    t set t.cjrdm='BC5D77315CA84C6C807988E3CD17E70D',t.cjrxm='系统管理员',t.cjrdwdm='141B1AFC7E634176BDA7DB7F491A9004',t.cjrdwmc='临时机构';
+update sys_qx_jsxx      t set t.cjrdm='BC5D77315CA84C6C807988E3CD17E70D',t.cjrxm='系统管理员',t.cjrdwdm='141B1AFC7E634176BDA7DB7F491A9004',t.cjrdwmc='临时机构';
+update sys_qx_qxxx      t set t.cjrdm='BC5D77315CA84C6C807988E3CD17E70D',t.cjrxm='系统管理员',t.cjrdwdm='141B1AFC7E634176BDA7DB7F491A9004',t.cjrdwmc='临时机构';
+update sys_qx_yhjsgl    t set t.cjrdm='BC5D77315CA84C6C807988E3CD17E70D',t.cjrxm='系统管理员',t.cjrdwdm='141B1AFC7E634176BDA7DB7F491A9004',t.cjrdwmc='临时机构';
+update sys_qx_yhxx      t set t.cjrdm='BC5D77315CA84C6C807988E3CD17E70D',t.cjrxm='系统管理员',t.cjrdwdm='141B1AFC7E634176BDA7DB7F491A9004',t.cjrdwmc='临时机构';
+update sys_sjgl_file    t set t.cjrdm='BC5D77315CA84C6C807988E3CD17E70D',t.cjrxm='系统管理员',t.cjrdwdm='141B1AFC7E634176BDA7DB7F491A9004',t.cjrdwmc='临时机构';
+update sys_sjgl_sjdx    t set t.cjrdm='BC5D77315CA84C6C807988E3CD17E70D',t.cjrxm='系统管理员',t.cjrdwdm='141B1AFC7E634176BDA7DB7F491A9004',t.cjrdwmc='临时机构';
+update sys_sjgl_sjzd    t set t.cjrdm='BC5D77315CA84C6C807988E3CD17E70D',t.cjrxm='系统管理员',t.cjrdwdm='141B1AFC7E634176BDA7DB7F491A9004',t.cjrdwmc='临时机构';
+update sys_sjgl_sjzt    t set t.cjrdm='BC5D77315CA84C6C807988E3CD17E70D',t.cjrxm='系统管理员',t.cjrdwdm='141B1AFC7E634176BDA7DB7F491A9004',t.cjrdwmc='临时机构';
+update sys_sjgl_tyzd    t set t.cjrdm='BC5D77315CA84C6C807988E3CD17E70D',t.cjrxm='系统管理员',t.cjrdwdm='141B1AFC7E634176BDA7DB7F491A9004',t.cjrdwmc='临时机构';
+update sys_sjgl_yhzdy   t set t.cjrdm='BC5D77315CA84C6C807988E3CD17E70D',t.cjrxm='系统管理员',t.cjrdwdm='141B1AFC7E634176BDA7DB7F491A9004',t.cjrdwmc='临时机构';
+update sys_xmgl_rwxx    t set t.cjrdm='BC5D77315CA84C6C807988E3CD17E70D',t.cjrxm='系统管理员',t.cjrdwdm='141B1AFC7E634176BDA7DB7F491A9004',t.cjrdwmc='临时机构';
+update sys_yxjk_jkrw    t set t.cjrdm='BC5D77315CA84C6C807988E3CD17E70D',t.cjrxm='系统管理员',t.cjrdwdm='141B1AFC7E634176BDA7DB7F491A9004',t.cjrdwmc='临时机构';
+update sys_yxjk_xx      t set t.cjrdm='BC5D77315CA84C6C807988E3CD17E70D',t.cjrxm='系统管理员',t.cjrdwdm='141B1AFC7E634176BDA7DB7F491A9004',t.cjrdwmc='临时机构';
+update sys_yxjk_zdysql  t set t.cjrdm='BC5D77315CA84C6C807988E3CD17E70D',t.cjrxm='系统管理员',t.cjrdwdm='141B1AFC7E634176BDA7DB7F491A9004',t.cjrdwmc='临时机构';
