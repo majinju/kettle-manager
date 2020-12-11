7bef15efcce7cef61f6857821df1b544b7d56140
majinju <jinjuma@yeah.net>
2020-12-11 17:02:23 +0800

    更新数据库信息，共享区调度部署完成

--- src/main/resources/sql/oracle/myNote.sql
+++ src/main/resources/sql/oracle/myNote.sql
@@ -1,12 +1,17 @@
 -- 远程导入数据
 --imp userid=sjsj/sjsj fromuser=sjsj touser=sjsj ignore=y file=d:\sjsj.dmp commit buffer=512000000
-
+--导出备份数据
+--exp sjsj/sjsj@127.0.0.1/mydb file=d:\sjsj.dmp owner=('sjsj')
 ----------密码加密解密-------
 select des_en('123456','123asdzxc') from dual;
 select des_en('zxcvbnm,.','123asdzxc') from dual;
 --数据载体
 --update sys_sjgl_sjzt t set t.mm=des_en(t.mm,'5zIcmw5qVZs=');
 select des_de(t.mm,'5zIcmw5qVZs='),t.* from sys_sjgl_sjzt t;
+--解密，改密码，加密
+update sys_sjgl_sjzt t set t.mm=des_de(t.mm,'5zIcmw5qVZs=') where t.mm is not null;
+select t.rowid,t.* from sys_sjgl_sjzt t;
+update sys_sjgl_sjzt t set t.mm=des_en(t.mm,'5zIcmw5qVZs=') where t.mm is not null;
 --用户
 --update sys_qx_yhxx t set t.yhmm=des_en(t.yhmm,'YA3EPe3fj/XrUtXDBr0y/Q==');
 select des_de(t.yhmm,'YA3EPe3fj/XrUtXDBr0y/Q=='),t.* from sys_qx_yhxx t;
@@ -17,7 +22,7 @@ select des_de(t.mm,'YA3EPe3fj/XrUtXDBr0y/Q=='),t.* from sys_qx_app t;
 --update sys_qx_fwq t set t.mm=des_en(t.mm,'YA3EPe3fj/XrUtXDBr0y/Q==') where t.mm is not null;
 select des_de(t.mm,'YA3EPe3fj/XrUtXDBr0y/Q=='),t.* from sys_qx_fwq t;
 
---修改一级密码
+--修改一级密码,修改一级密码需要修改应用配置文件和kettle的配置文件。
 --数据载体
 select des_en(des_de(t.mm,des_en('123456','123asdzxc')),des_en('123456','1qaz2wsx3')) xmm,des_de(t.mm,'5zIcmw5qVZs=') zmm,t.* from sys_sjgl_sjzt t where t.mm is not null;
 --用户
@@ -42,6 +47,7 @@ update sys_sjgl_grbj t
        t.cjrdwdm = '141B1AFC7E634176BDA7DB7F491A9004';
 
 --因为Navicat导出没有导出主键，需要单独执行此语句建立主键
+/*
 alter table JCGA_JCYG_JCXX  add constraint PK_JCGA_JCYG_JCXX  primary key (ID);
 alter table JCGA_JCYG_SHGX  add constraint PK_JCGA_JCYG_SHGX  primary key (ID);
 alter table SYS_LOG_FWZR    add constraint PK_SYS_LOG_FWZR    primary key (ID);
@@ -70,3 +76,4 @@ alter table SYS_YXJK_XX     add constraint PK_SYS_YXJK_XX     primary key (ID);
 alter table SYS_YXJK_ZDYSQL add constraint PK_SYS_YXJK_ZDYSQL primary key (ID);
 alter table sys_sjgl_blob add constraint PK_sys_sjgl_blob primary key (ID);
 alter table SYS_SJGL_GRBJ add constraint PK_SYS_SJGL_GRBJ primary key (ID);
+*/
