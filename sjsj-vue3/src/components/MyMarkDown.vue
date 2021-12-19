db6591c12ff6ed1e74a70d566abcd28491fb341b
majinju <jinjuma@yeah.net>
2021-12-19 20:25:25 +0800

    支持图片和文件上传

--- sjsj-vue3/src/components/MyMarkDown.vue
+++ sjsj-vue3/src/components/MyMarkDown.vue
@@ -1,6 +1,6 @@
 <template>
   <v-md-editor v-model="myData.value"
-               left-toolbar="undo redo clear | codeType h bold italic strikethrough quote | ul ol table hr | link image code | save"
+               left-toolbar="undo redo clear | codeType h bold italic strikethrough quote | ul ol table hr | link image uploadFile code "
                :toolbar="myData.toolbar"
                @change="valChange"
                :disabled-menus="[]"
@@ -11,6 +11,9 @@
 
 import {defineComponent, reactive, watch} from "vue";
 import {isEmpty, jsonFormat} from "@/utils/common"
+import axios from "@/axios";
+import {ElMessage} from "element-plus";
+import {VXETable} from "@majinju/vxe-table";
 
 export default defineComponent({
   name: "MyMarkDown",
@@ -34,6 +37,37 @@ export default defineComponent({
       value:"",
       codeType:props.codeType,
       toolbar:{
+        uploadFile: {
+          title: '上传文件',
+          icon: 'el-icon-upload',
+          async action(editor) {
+            const { file,files } = await VXETable.readFile({
+              multiple: true
+            })
+            const formBody = new FormData();
+            formBody.append("sys.authCode", "QTQX");
+            formBody.append("sys.cllx", "upload");
+            for(let i=0;i<files.length;i++){
+              formBody.append("files", files[i]);
+            }
+            axios.upload(formBody).then((res) => {
+              if (res.status) {
+                editor.insert(function (selected) {
+                  let content = "";
+                  for(let i=0;i<res.data.length;i++){
+                    const f = res.data[i];
+                    content += "1. ["+f.wjm+"]("+axios.getBaseURL()+'?sjdx.dxdm=SYS_QX_QTQX&sys.cllx=download&yobj.id='+f.id+")\n"
+                  }
+                  return {
+                    text: `${content}`,
+                    selected: "",
+                  };
+                });
+                ElMessage.info("上传成功："+res.data.length);
+              }
+            });
+          }
+        },
         codeType: {
           title: '代码风格',
           icon: 'el-icon-price-tag',
@@ -90,6 +124,10 @@ export default defineComponent({
         }
       }
     });
+    /**
+     * 设置值
+     * @param val 值
+     */
     const setValue = (val) => {
       if(!val){
         //设置默认值
@@ -124,9 +162,36 @@ export default defineComponent({
         context.emit('update:modelValue',val.replace(/```.*\n/,"").replace("\n```",""))
       }
     }
+    /**
+     * 上传图片
+     * @param event 事件
+     * @param insertImage 插入图片方法
+     * @param files 文件列表
+     */
+    const uploadImage = (event, insertImage, files) => {
+      const formBody = new FormData();
+      formBody.append("sys.authCode", "QTQX");
+      formBody.append("sys.cllx", "upload");
+      for(let i=0;i<files.length;i++){
+        formBody.append("files", files[i]);
+      }
+      axios.upload(formBody).then((res) => {
+        if (res.status) {
+          const f = res.data[0];
+          insertImage({
+            url: axios.getBaseURL()+'?sjdx.dxdm=SYS_QX_QTQX&sys.cllx=download&yobj.id='+f.id,
+            desc: f.wjm,
+            // width: 'auto',
+            // height: 'auto',
+          });
+          ElMessage.info("上传图片成功");
+        }
+      });
+    }
     return {
       myData,
-      valChange
+      valChange,
+      uploadImage
     }
   }
 })
