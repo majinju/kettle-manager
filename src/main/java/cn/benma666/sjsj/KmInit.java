2b7d075c96ad169325c0ef81f33620bfc702b5ae
马进举 <jinjuma@yeah.net>
2023-05-20 00:27:07 +0800

    在客户端能正常使用插件，能正常加载jobManager，相关配置已经添加到项目中，myutils依赖包要采用noweb分支版本 持续改进kettle管理，还得加油哦

--- src/main/java/cn/benma666/sjsj/KmInit.java
+++ src/main/java/cn/benma666/sjsj/KmInit.java
@@ -1,6 +1,7 @@
 package cn.benma666.sjsj;
 
 import cn.benma666.iframe.BasicObject;
+import cn.benma666.iframe.Conf;
 import cn.benma666.kettle.job.JobManager;
 import cn.benma666.sjsj.myutils.ThreadPool;
 import org.springframework.boot.ApplicationArguments;
@@ -16,6 +17,10 @@ import org.springframework.stereotype.Component;
 public class KmInit extends BasicObject implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) {
+        //进行系统属性设置
+        for (String p:Conf.getVal("web.init.property").split(",")){
+            System.setProperty(p,Conf.getVal(p));
+        }
         //智能交换-应用初始化启动
         ThreadPool.use().run(JobManager::init);
     }
